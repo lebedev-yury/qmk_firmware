af80847eeff16ced49ca3b5ebbbf5ccf0aca0882
Yury Lebedev <lebedev.yurii@gmail.com>
2017-04-06 15:49:05 +0200

    Add version 2.0 of my personal layout

--- keyboards/ergodox/keymaps/lebedev/keymap.c
+++ keyboards/ergodox/keymaps/lebedev/keymap.c
@@ -14,49 +14,49 @@ enum {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = KEYMAP(
-    TD(TD_VIM_MACRO), KC_1,         KC_2,    KC_3,    KC_4,    KC_5, TG(SYMB),
-    KC_TAB,           KC_Q,         KC_W,    KC_E,    KC_R,    KC_T, KC_EQUAL,
-    CTL_T(KC_ESCAPE), KC_A,         KC_S,    KC_D,    KC_F,    KC_G,
-    KC_LSHIFT,        KC_Z,         KC_X,    KC_C,    KC_V,    KC_B, KC_MINUS,
-    ALL_T(KC_NO),     MEH_T(KC_NO), KC_LCTL, KC_LALT, KC_LGUI,
+    TD(TD_VIM_MACRO), KC_1,           KC_2,    KC_3,    KC_4,    KC_5, TG(SYMB),
+    KC_TAB,           KC_Q,           KC_W,    KC_E,    KC_R,    KC_T, KC_EQUAL,
+    CTL_T(KC_ESCAPE), KC_A,           KC_S,    KC_D,    KC_F,    KC_G,
+    KC_LSHIFT,        LT(SYMB, KC_Z), KC_X,    KC_C,    KC_V,    KC_B, KC_MINUS,
+    ALL_T(KC_NO),     MEH_T(KC_NO),   KC_LCTL, KC_LALT, KC_LGUI,
 
-                                                          KC_0,      KC_DLR,
-                                                                     MO(MDIA),
-                                                KC_SPACE, KC_BSPACE, MO(SYMB),
+                                                KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,
+                                                                     KC_MEDIA_PLAY_PAUSE,
+                                                KC_SPACE, KC_BSPACE, KC_0,
 
 
-    TG(MDIA), KC_6, KC_7,    KC_8,     KC_9,   KC_0,      KC_AT,
-    KC_CAPS,  KC_Y, KC_U,    KC_I,     KC_O,   KC_P,      KC_BSLASH,
-              KC_H, KC_J,    KC_K,     KC_L,   KC_SCOLON, KC_QUOTE,
-    KC_UNDS,  KC_N, KC_M,    KC_COMMA, KC_DOT, KC_SLASH,  KC_RSHIFT,
-                    KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT,  ALL_T(KC_NO),
+    TG(MDIA), KC_6, KC_7,    KC_8,     KC_9,        KC_0,               KC_AT,
+    KC_CAPS,  KC_Y, KC_U,    KC_I,     KC_O,        KC_P,               KC_BSLASH,
+              KC_H, KC_J,    KC_K,     KC_L,        KC_SCOLON,          LT(MDIA, KC_QUOTE),
+    KC_UNDS,  KC_N, KC_M,    KC_COMMA, KC_DOT,      LT(SYMB, KC_SLASH), KC_RSHIFT,
+                    KC_LGUI, KC_LALT,  KC_LBRACKET, KC_RBRACKET,        ALL_T(KC_NO),
 
-    KC_ASTR,  KC_PERC,
-    MO(MDIA),
-    MO(SYMB), CTL_T(KC_ESCAPE), KC_ENTER
+    KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP,
+    KC_AUDIO_MUTE,
+    KC_DLR,            CTL_T(KC_ESCAPE), KC_ENTER
   ),
 
   [SYMB] = KEYMAP(
-    M(0),             KC_F1,   KC_F2,   KC_F3,       KC_F4,       KC_F5,    KC_TRNS,
-    KC_TRNS,          KC_EXLM, KC_AT,   KC_LCBR,     KC_RCBR,     KC_PIPE,  KC_TRNS,
-    CTL_T(KC_ESCAPE), KC_HASH, KC_DLR,  KC_LPRN,     KC_RPRN,     KC_GRAVE,
-    KC_TRNS,          KC_PERC, KC_CIRC, KC_LBRACKET, KC_RBRACKET, KC_TILD,  KC_TRNS,
-    KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,
+    M(0),    KC_F1,   KC_F2,   KC_F3,       KC_F4,       KC_F5,    KC_TRNS,
+    KC_TRNS, KC_EXLM, KC_AT,   KC_HASH,     KC_DLR,      KC_PERC,  KC_TRNS,
+    KC_TRNS, KC_LCBR, KC_RCBR, KC_LPRN,     KC_RPRN,     KC_GRAVE,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRACKET, KC_RBRACKET, KC_TILD,  KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,
 
                                                                   KC_TRNS, KC_TRNS,
                                                                            KC_TRNS,
-                                                     KC_SPACE,  KC_BSPACE, KC_TRNS,
+                                                         KC_TRNS, KC_TRNS, KC_TRNS,
 
 
-    KC_TRNS, KC_F6,   KC_F7,  KC_F8, KC_F9,    KC_F10,    KC_F11,
-    KC_TRNS, KC_UP,   KC_7,   KC_8,  KC_9,     KC_ASTR,   KC_F12,
-             KC_DOWN, KC_4,   KC_5,  KC_6,     KC_PLUS,   KC_TRNS,
-    KC_TRNS, KC_AMPR, KC_1,   KC_2,  KC_3,     KC_BSLASH, KC_TRNS,
-                      KC_DOT, KC_0,  KC_EQUAL, KC_TRNS,   KC_TRNS,
+    KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,    KC_F11,
+    KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINUS, KC_EQUAL,  KC_F12,
+             KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_PLUS,   KC_TRNS,
+    KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_TRNS,   KC_TRNS,
+                      KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS,
 
     KC_TRNS, KC_TRNS,
     KC_TRNS,
-    KC_TRNS, CTL_T(KC_ESCAPE), KC_ENTER
+    KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [MDIA] = KEYMAP(
@@ -71,15 +71,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             KC_MS_BTN1, KC_MS_BTN2, KC_TRNS,
 
 
-    KC_TRNS, KC_TRNS, KC_AUDIO_MUTE,       KC_AUDIO_VOL_DOWN,   KC_AUDIO_VOL_UP,     KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, KC_TRNS, KC_TRNS,
-             KC_LEFT, KC_DOWN,             KC_UP,               KC_RIGHT,            KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS,             KC_TRNS,             KC_TRNS, KC_TRNS,
-                      KC_TRNS,             KC_TRNS,             KC_TRNS,             KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS,    KC_TRNS,    KC_EQUAL, KC_KP_SLASH, KC_KP_ASTERISK, KC_TRNS,
+    KC_TRNS, KC_TRNS,    KC_KP_7,    KC_KP_8,  KC_KP_9,     KC_KP_MINUS,    KC_TRNS,
+             KC_TRNS,    KC_KP_4,    KC_KP_5,  KC_KP_6,     KC_KP_PLUS,     KC_TRNS,
+    KC_TRNS, KC_TRNS,    KC_KP_1,    KC_KP_2,  KC_KP_3,     KC_TRNS,        KC_TRNS,
+                         KC_TRNS,    KC_KP_0,  KC_KP_DOT,   KC_TRNS,        KC_TRNS,
 
     KC_TRNS, KC_TRNS,
     KC_TRNS,
-    KC_TRNS, KC_WWW_BACK, KC_WWW_FORWARD
+    KC_TRNS, KC_TRNS, KC_TRNS
   ),
 };
 
