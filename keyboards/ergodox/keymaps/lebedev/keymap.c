d1d6cde802742f9d82108152179f4178826edac4
Yury Lebedev <lebedev.yurii@gmail.com>
2017-04-07 09:55:50 +0200

    Move symb layer toggle to default position

--- keyboards/ergodox/keymaps/lebedev/keymap.c
+++ keyboards/ergodox/keymaps/lebedev/keymap.c
@@ -9,27 +9,28 @@
 
 // Tap Dance keys
 enum {
-  TD_VIM_MACRO = 0
+  TD_VIM_MACRO = 0,
+  TD_LAYERS_MACRO = 1
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = KEYMAP(
-    TD(TD_VIM_MACRO), KC_1,           KC_2,    KC_3,    KC_4,    KC_5, TG(SYMB),
-    KC_TAB,           KC_Q,           KC_W,    KC_E,    KC_R,    KC_T, KC_EQUAL,
-    CTL_T(KC_ESCAPE), KC_A,           KC_S,    KC_D,    KC_F,    KC_G,
-    KC_LSHIFT,        LT(SYMB, KC_Z), KC_X,    KC_C,    KC_V,    KC_B, KC_MINUS,
-    ALL_T(KC_NO),     MEH_T(KC_NO),   KC_LCTL, KC_LALT, KC_LGUI,
+    TD(TD_VIM_MACRO), KC_1,         KC_2,    KC_3,    KC_4,    KC_5, TD(TD_LAYERS_MACRO),
+    KC_TAB,           KC_Q,         KC_W,    KC_E,    KC_R,    KC_T, KC_LBRACKET,
+    CTL_T(KC_ESCAPE), KC_A,         KC_S,    KC_D,    KC_F,    KC_G,
+    KC_LSHIFT,        KC_Z,         KC_X,    KC_C,    KC_V,    KC_B, MO(SYMB),
+    ALL_T(KC_NO),     MEH_T(KC_NO), KC_LCTL, KC_LALT, KC_LGUI,
 
                                                 KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,
                                                                      KC_MEDIA_PLAY_PAUSE,
                                                 KC_SPACE, KC_BSPACE, KC_0,
 
 
-    TG(MDIA), KC_6, KC_7,    KC_8,     KC_9,        KC_0,               KC_AT,
-    KC_CAPS,  KC_Y, KC_U,    KC_I,     KC_O,        KC_P,               KC_BSLASH,
-              KC_H, KC_J,    KC_K,     KC_L,        KC_SCOLON,          LT(MDIA, KC_QUOTE),
-    KC_UNDS,  KC_N, KC_M,    KC_COMMA, KC_DOT,      LT(SYMB, KC_SLASH), KC_RSHIFT,
-                    KC_LGUI, KC_LALT,  KC_LBRACKET, KC_RBRACKET,        ALL_T(KC_NO),
+    KC_CAPS,     KC_6, KC_7,    KC_8,     KC_9,    KC_0,      KC_AT,
+    KC_RBRACKET, KC_Y, KC_U,    KC_I,     KC_O,    KC_P,      KC_BSLASH,
+                 KC_H, KC_J,    KC_K,     KC_L,    KC_SCOLON, LT(MDIA, KC_QUOTE),
+    MO(SYMB),    KC_N, KC_M,    KC_COMMA, KC_DOT,  KC_SLASH,  KC_RSHIFT,
+                       KC_LGUI, KC_LALT,  KC_LPRN, KC_RPRN,   ALL_T(KC_NO),
 
     KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP,
     KC_AUDIO_MUTE,
@@ -40,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     M(0),    KC_F1,   KC_F2,   KC_F3,       KC_F4,       KC_F5,    KC_TRNS,
     KC_TRNS, KC_EXLM, KC_AT,   KC_HASH,     KC_DLR,      KC_PERC,  KC_TRNS,
     KC_TRNS, KC_LCBR, KC_RCBR, KC_LPRN,     KC_RPRN,     KC_GRAVE,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRACKET, KC_RBRACKET, KC_TILD,  KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_PIPE, KC_LBRACKET, KC_RBRACKET, KC_TILD,  KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,
 
                                                                   KC_TRNS, KC_TRNS,
@@ -50,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,    KC_F11,
     KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINUS, KC_EQUAL,  KC_F12,
-             KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_PLUS,   KC_TRNS,
+             KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_UNDS,   KC_PLUS,
     KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_TRNS,   KC_TRNS,
                       KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS,
 
@@ -171,8 +172,27 @@ void vim_macro_dance_finished(qk_tap_dance_state_t *state, void *user_data) {
   reset_tap_dance(state);
 }
 
+void layers_macro_dance_finished(qk_tap_dance_state_t *state, void *user_data) {
+  switch (state->count) {
+  case 1:
+    layer_off(SYMB);
+    layer_off(MDIA);
+    break;
+  case 2:
+    layer_on(SYMB);
+    layer_off(MDIA);
+    break;
+  case 3:
+    layer_off(SYMB);
+    layer_on(MDIA);
+    break;
+  }
+  reset_tap_dance(state);
+}
+
 // Tap Dance Definitions
 
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_VIM_MACRO] = ACTION_TAP_DANCE_FN(vim_macro_dance_finished),
+  [TD_LAYERS_MACRO] = ACTION_TAP_DANCE_FN(layers_macro_dance_finished)
 };
