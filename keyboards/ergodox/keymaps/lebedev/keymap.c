e323c06738d6624d24d25b20479d6a61ee6edcba
Yury Lebedev <lebedev.yurii@gmail.com>
2017-04-10 09:38:54 +0200

    Move symbol layer toggle keys

--- keyboards/ergodox/keymaps/lebedev/keymap.c
+++ keyboards/ergodox/keymaps/lebedev/keymap.c
@@ -15,22 +15,22 @@ enum {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = KEYMAP(
-    TD(TD_VIM_MACRO), KC_1,         KC_2,    KC_3,    KC_4,    KC_5, TD(TD_LAYERS_MACRO),
-    KC_TAB,           KC_Q,         KC_W,    KC_E,    KC_R,    KC_T, KC_LBRACKET,
-    CTL_T(KC_ESCAPE), KC_A,         KC_S,    KC_D,    KC_F,    KC_G,
-    KC_LSHIFT,        KC_Z,         KC_X,    KC_C,    KC_V,    KC_B, MO(SYMB),
-    ALL_T(KC_NO),     MEH_T(KC_NO), KC_LCTL, KC_LALT, KC_LGUI,
+    TD(TD_VIM_MACRO), KC_1,     KC_2,         KC_3,    KC_4,    KC_5, TD(TD_LAYERS_MACRO),
+    KC_TAB,           KC_Q,     KC_W,         KC_E,    KC_R,    KC_T, KC_LBRACKET,
+    CTL_T(KC_ESCAPE), KC_A,     KC_S,         KC_D,    KC_F,    KC_G,
+    KC_LSHIFT,        KC_Z,     KC_X,         KC_C,    KC_V,    KC_B, KC_MINUS,
+    ALL_T(KC_NO),     MO(SYMB), MEH_T(KC_NO), KC_LALT, KC_LGUI,
 
-                                                KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,
-                                                                     KC_MEDIA_PLAY_PAUSE,
-                                                KC_SPACE, KC_BSPACE, KC_0,
+                                         KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,
+                                                              KC_MEDIA_PLAY_PAUSE,
+                                         KC_SPACE, KC_BSPACE, KC_0,
 
 
-    KC_CAPS,     KC_6, KC_7,    KC_8,     KC_9,    KC_0,      KC_AT,
-    KC_RBRACKET, KC_Y, KC_U,    KC_I,     KC_O,    KC_P,      KC_BSLASH,
-                 KC_H, KC_J,    KC_K,     KC_L,    KC_SCOLON, LT(MDIA, KC_QUOTE),
-    MO(SYMB),    KC_N, KC_M,    KC_COMMA, KC_DOT,  KC_SLASH,  KC_RSHIFT,
-                       KC_LGUI, KC_LALT,  KC_LPRN, KC_RPRN,   ALL_T(KC_NO),
+    KC_CAPS,     KC_6, KC_7,    KC_8,     KC_9,         KC_0,      KC_AT,
+    KC_RBRACKET, KC_Y, KC_U,    KC_I,     KC_O,         KC_P,      KC_BSLASH,
+                 KC_H, KC_J,    KC_K,     KC_L,         KC_SCOLON, LT(MDIA, KC_QUOTE),
+    KC_UNDS,     KC_N, KC_M,    KC_COMMA, KC_DOT,       KC_SLASH,  KC_RSHIFT,
+                       KC_LGUI, KC_LALT,  MEH_T(KC_NO), MO(SYMB),  ALL_T(KC_NO),
 
     KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP,
     KC_AUDIO_MUTE,
