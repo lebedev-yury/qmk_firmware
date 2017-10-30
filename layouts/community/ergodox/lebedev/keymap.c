139d6268859ee511d513c2c9243856b7eb325e32
Yury Lebedev <lebedev.yurii@gmail.com>
2017-10-30 09:29:40 +0100

    Update Ergodox layout

--- layouts/community/ergodox/lebedev/keymap.c
+++ layouts/community/ergodox/lebedev/keymap.c
@@ -5,7 +5,7 @@
 
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
-#define MDIA 2 // media keys
+#define NUMB 2 // media keys
 
 // Tap Dance keys
 enum {
@@ -15,11 +15,11 @@ enum {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_ergodox(
-    TD(TD_VIM_MACRO), KC_1,     KC_2,         KC_3,    KC_4,    KC_5, TD(TD_LAYERS_MACRO),
-    KC_TAB,           KC_Q,     KC_W,         KC_E,    KC_R,    KC_T, KC_F12,
-    CTL_T(KC_ESCAPE), KC_A,     KC_S,         KC_D,    KC_F,    KC_G,
-    KC_LSPO,          KC_Z,     KC_X,         KC_C,    KC_V,    KC_B, KC_LBRACKET,
-    ALL_T(KC_NO),     MO(SYMB), KC_GRAVE,     KC_LALT, KC_LGUI,
+    TD(TD_VIM_MACRO), KC_1,     KC_2,    KC_3,    KC_4,    KC_5, TD(TD_LAYERS_MACRO),
+    KC_TAB,           KC_Q,     KC_W,    KC_E,    KC_R,    KC_T, KC_F12,
+    CTL_T(KC_ESCAPE), KC_A,     KC_S,    KC_D,    KC_F,    KC_G,
+    KC_LSPO,          KC_Z,     KC_X,    KC_C,    KC_V,    KC_B, KC_LBRACKET,
+    ALL_T(KC_NO),     MO(NUMB), KC_LALT, KC_LGUI, MO(SYMB),
 
                                          KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,
                                                               KC_MEDIA_PLAY_PAUSE,
@@ -28,9 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     KC_CAPS,     KC_6, KC_7,     KC_8,     KC_9,    KC_0,      KC_MINUS,
     KC_F12,      KC_Y, KC_U,     KC_I,     KC_O,    KC_P,      KC_BSLASH,
-                 KC_H, KC_J,     KC_K,     KC_L,    KC_SCOLON, LT(MDIA, KC_QUOTE),
+                 KC_H, KC_J,     KC_K,     KC_L,    KC_SCOLON, CTL_T(KC_QUOTE),
     KC_RBRACKET, KC_N, KC_M,     KC_COMMA, KC_DOT,  KC_SLASH,  KC_RSPC,
-                       KC_LGUI,  KC_LALT,  KC_TILD, MO(SYMB),  ALL_T(KC_NO),
+                       MO(SYMB), KC_LGUI,  KC_LALT, MO(NUMB),  ALL_T(KC_NO),
 
     KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP,
     KC_AUDIO_MUTE,
@@ -38,19 +38,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [SYMB] = LAYOUT_ergodox(
-    M(0),    KC_F1,   KC_F2,   KC_F3,       KC_F4,       KC_F5,    KC_TRNS,
-    KC_TRNS, KC_EXLM, KC_AT,   KC_HASH,     KC_DLR,      KC_PERC,  KC_TRNS,
-    KC_TRNS, KC_LPRN, KC_RPRN, KC_LCBR,     KC_RCBR,     KC_GRAVE,
-    KC_TRNS, KC_TRNS, KC_PIPE, KC_LBRACKET, KC_RBRACKET, KC_TILD,  KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,
+    M(0),    KC_F1,    KC_F2,   KC_F3,       KC_F4,       KC_F5,    KC_F6,
+    KC_TRNS, KC_EXLM,  KC_AT,   KC_HASH,     KC_DLR,      KC_PERC,  KC_TRNS,
+    KC_TRNS, KC_GRAVE, KC_LPRN, KC_LCBR,     KC_RCBR,     KC_RPRN,
+    KC_TRNS, KC_TILD,  KC_PIPE, KC_LBRACKET, KC_RBRACKET, KC_TRNS,  KC_TRNS,
+    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,     KC_TRNS,
 
                                                                   KC_TRNS, KC_TRNS,
                                                                            KC_TRNS,
                                                          KC_TRNS, KC_TRNS, KC_TRNS,
 
 
-    KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,    KC_F11,
-    KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINUS, KC_EQUAL,  KC_F12,
+    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,    KC_F13,
+    KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINUS, KC_EQUAL,  KC_TRNS,
              KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_UNDS,   KC_PLUS,
     KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_TRNS,   KC_TRNS,
                       KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS,
@@ -60,23 +60,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
-  [MDIA] = LAYOUT_ergodox(
-    KC_TRNS, KC_F14,  KC_F15,     KC_TRNS,    KC_TRNS,     KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS,    KC_MS_UP,   KC_TRNS,     KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_TRNS,
-    KC_TRNS, M(1),    M(2),       M(3),       KC_TRNS,     KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,
+  [NUMB] = LAYOUT_ergodox(
+    KC_TRNS, KC_F14,  KC_F15,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_TRNS,
+    KC_TRNS, KC_PIPE, KC_AMPR, KC_ASTR, KC_SLASH, KC_PERC,
+    KC_TRNS, M(1),    M(2),    M(3),    KC_TRNS,  KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
                                                            KC_TRNS, KC_TRNS,
                                                                     KC_TRNS,
-                                            KC_MS_BTN1, KC_MS_BTN2, KC_TRNS,
+                                                  KC_TRNS, KC_TRNS, KC_TRNS,
 
 
-    KC_TRNS, KC_TRNS,    KC_TRNS,    KC_EQUAL, KC_KP_SLASH, KC_KP_ASTERISK, KC_TRNS,
-    KC_TRNS, KC_TRNS,    KC_KP_7,    KC_KP_8,  KC_KP_9,     KC_KP_MINUS,    KC_TRNS,
-             KC_TRNS,    KC_KP_4,    KC_KP_5,  KC_KP_6,     KC_KP_PLUS,     KC_TRNS,
-    KC_TRNS, KC_TRNS,    KC_KP_1,    KC_KP_2,  KC_KP_3,     KC_TRNS,        KC_TRNS,
-                         KC_TRNS,    KC_KP_0,  KC_KP_DOT,   KC_TRNS,        KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_6,    KC_7,    KC_8,     KC_9,     KC_0,    KC_TRNS,
+             KC_CIRC, KC_PLUS, KC_MINUS, KC_EQUAL, KC_EXLM, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,
+                      KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,
 
     KC_TRNS, KC_TRNS,
     KC_TRNS,
@@ -176,15 +176,15 @@ void layers_macro_dance_finished(qk_tap_dance_state_t *state, void *user_data) {
   switch (state->count) {
   case 1:
     layer_off(SYMB);
-    layer_off(MDIA);
+    layer_off(NUMB);
     break;
   case 2:
     layer_on(SYMB);
-    layer_off(MDIA);
+    layer_off(NUMB);
     break;
   case 3:
     layer_off(SYMB);
-    layer_on(MDIA);
+    layer_on(NUMB);
     break;
   }
   reset_tap_dance(state);
