620aab1564aaa543204f1e5b45d986a9f3446319
Yury Lebedev <yury.lebedev@daimler.com>
2018-02-27 10:16:24 +0100

    Add tmux leader key

--- layouts/community/ergodox/lebedev/keymap.c
+++ layouts/community/ergodox/lebedev/keymap.c
@@ -16,7 +16,7 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_ergodox(
     TD(TD_VIM_MACRO), KC_1,     KC_2,    KC_3,    KC_4,    KC_5, TD(TD_LAYERS_MACRO),
-    KC_TAB,           KC_Q,     KC_W,    KC_E,    KC_R,    KC_T, KC_F12,
+    KC_TAB,           KC_Q,     KC_W,    KC_E,    KC_R,    KC_T, LCTL(KC_B),
     CTL_T(KC_ESCAPE), KC_A,     KC_S,    KC_D,    KC_F,    KC_G,
     KC_LSPO,          KC_Z,     KC_X,    KC_C,    KC_V,    KC_B, KC_LBRACKET,
     ALL_T(KC_NO),     MO(NUMB), KC_LALT, KC_LGUI, MO(SYMB),
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     KC_CAPS,     KC_6, KC_7,     KC_8,     KC_9,    KC_0,      KC_MINUS,
-    KC_F12,      KC_Y, KC_U,     KC_I,     KC_O,    KC_P,      KC_BSLASH,
+    LCTL(KC_B),  KC_Y, KC_U,     KC_I,     KC_O,    KC_P,      KC_BSLASH,
                  KC_H, KC_J,     KC_K,     KC_L,    KC_SCOLON, CTL_T(KC_QUOTE),
     KC_RBRACKET, KC_N, KC_M,     KC_COMMA, KC_DOT,  KC_SLASH,  KC_RSPC,
                        MO(SYMB), KC_LGUI,  KC_LALT, MO(NUMB),  ALL_T(KC_NO),
