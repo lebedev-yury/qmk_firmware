60dad46ee9714b1f6f0b6c03bea787c2be948a4e
Yury Lebedev <yury.lebedev@daimler.com>
2018-08-25 12:24:36 +0200

    Update planck layout

--- keyboards/planck/keymaps/lebedev/keymap.c
+++ keyboards/planck/keymaps/lebedev/keymap.c
@@ -41,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Hyper| Numb | Alt  | Cmd  | Symb | Space| Enter| Symb | Cmd  | CAPS | Numb | Tmux |
+ * | Numb | Tmux | Alt  | Cmd  | Symb | Space| Enter| Symb | Cmd  | CAPS | Tmux | Numb |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_TAB,        KC_Q,      KC_W,    KC_E,    KC_R, KC_T,    KC_Y,      KC_U, KC_I,    KC_O,    KC_P,      KC_BSPC},
-  {CTL_T(KC_ESC), KC_A,      KC_S,    KC_D,    KC_F, KC_G,    KC_H,      KC_J, KC_K,    KC_L,    KC_SCLN,   KC_QUOT},
-  {KC_LSPO,       KC_Z,      KC_X,    KC_C,    KC_V, KC_B,    KC_N,      KC_M, KC_COMM, KC_DOT,  KC_SLSH,   KC_RSPC},
-  {ALL_T(KC_NO),  TT(_NUMB), KC_LALT, KC_LGUI, SYMB, KC_SPC,  KC_ENTER,  SYMB, KC_LGUI, KC_CAPS, TT(_NUMB), LCTL(KC_B)}
+  {KC_TAB,        KC_Q,   KC_W,    KC_E,    KC_R, KC_T,    KC_Y,      KC_U, KC_I,    KC_O,    KC_P,       KC_BSPC},
+  {CTL_T(KC_ESC), KC_A,   KC_S,    KC_D,    KC_F, KC_G,    KC_H,      KC_J, KC_K,    KC_L,    KC_SCLN,    KC_QUOT},
+  {KC_LSPO,       KC_Z,   KC_X,    KC_C,    KC_V, KC_B,    KC_N,      KC_M, KC_COMM, KC_DOT,  KC_SLSH,    KC_RSPC},
+  {TT(_NUMB), LCTL(KC_B), KC_LALT, KC_LGUI, SYMB, KC_SPC,  KC_ENTER,  SYMB, KC_LGUI, KC_CAPS, LCTL(KC_B), TT(_NUMB)}
 },
 
 /* Symbols
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_SYMB] = {
-  {_______, KC_EXLM,  KC_AT,   KC_HASH,     KC_DLR,      KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINUS, KC_EQUAL, KC_BSLASH},
+  {KC_DEL,  KC_EXLM,  KC_AT,   KC_HASH,     KC_DLR,      KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINUS, KC_EQUAL, KC_BSLASH},
   {_______, KC_GRAVE, KC_LPRN, KC_LCBR,     KC_RCBR,     KC_RPRN, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_UNDS,  KC_PLUS},
   {_______, KC_TILD,  KC_PIPE, KC_LBRACKET, KC_RBRACKET, _______, _______, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,   _______},
   {_______, _______,  _______, _______,     _______,     _______, _______, _______, _______, _______,  _______,  _______}
