ff7d300965d072cf635f9f06ac734fb9aed2b7de
Yury Lebedev <lebedev.yurii@gmail.com>
2018-08-30 21:18:32 +0200

    Remove tap toggle for number layer on planck

--- keyboards/planck/keymaps/lebedev/keymap.c
+++ keyboards/planck/keymaps/lebedev/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,        KC_Q,   KC_W,    KC_E,    KC_R, KC_T,    KC_Y,      KC_U, KC_I,    KC_O,    KC_P,       KC_BSPC},
   {CTL_T(KC_ESC), KC_A,   KC_S,    KC_D,    KC_F, KC_G,    KC_H,      KC_J, KC_K,    KC_L,    KC_SCLN,    KC_QUOT},
   {KC_LSPO,       KC_Z,   KC_X,    KC_C,    KC_V, KC_B,    KC_N,      KC_M, KC_COMM, KC_DOT,  KC_SLSH,    KC_RSPC},
-  {TT(_NUMB), LCTL(KC_B), KC_LALT, KC_LGUI, SYMB, KC_SPC,  KC_ENTER,  SYMB, KC_LGUI, KC_CAPS, LCTL(KC_B), TT(_NUMB)}
+  {NUMB,      LCTL(KC_B), KC_LALT, KC_LGUI, SYMB, KC_SPC,  KC_ENTER,  SYMB, KC_LGUI, KC_CAPS, LCTL(KC_B), NUMB}
 },
 
 /* Symbols
